02b9ab8d682d2a3299339571f57b3a91bfd33fd5
Berkay-Disli <berkaydisli.work@gmail.com>
2022-05-08 23:31:42 +0300

    Initial Commit

---
+++ CoinsAPI/CoinsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsAPIApp.swift
+//  CoinsAPI
+//
+//  Created by Berkay Disli on 8.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoinsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
